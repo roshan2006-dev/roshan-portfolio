1a1797b9f14f268ed44b70bdcfac9df7875e00fc
roshan2006-dev <roshanmaahi786@gmail.com>
2026-03-12 23:08:24 +0530

    Uncomment Environment for 3D spheres

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -1,7 +1,7 @@
 import * as THREE from "three";
 import { useRef, useMemo, useState, useEffect } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
-//import { Environment } from "@react-three/drei";
+import { Environment } from "@react-three/drei";
 import { EffectComposer, N8AO } from "@react-three/postprocessing";
 import {
   BallCollider,
@@ -198,11 +198,11 @@ const TechStack = () => {
             />
           ))}
         </Physics>
-        {/* <Environment
+        <Environment
           files="/models/char_enviorment.hdr"
           environmentIntensity={0.5}
           environmentRotation={[0, 4, 2]}
-        /> */}
+        /> 
         <EffectComposer enableNormalPass={false}>
           <N8AO color="#0f002c" aoRadius={2} intensity={1.15} />
         </EffectComposer>

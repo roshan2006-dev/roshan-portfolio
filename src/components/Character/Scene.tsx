6d70701c0d3400f031cb21f8375e6375876477f3
roshan2006-dev <roshanmaahi786@gmail.com>
2026-03-12 22:48:26 +0530

    Fix Scene.tsx with proper code

--- src/components/Character/Scene.tsx
+++ src/components/Character/Scene.tsx
@@ -1,8 +1,5 @@
-# Overwrite Scene.tsx with empty component
-@"
 const Scene = () => {
   return null;
 };
 
 export default Scene;
-"@ | Out-File -FilePath src/components/Character/Scene.tsx -Encoding utf8

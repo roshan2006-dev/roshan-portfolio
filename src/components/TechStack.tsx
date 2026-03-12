e90b4da8432f5afb897b4297f669137ef39cf1cd
roshan2006-dev <roshanmaahi786@gmail.com>
2026-03-12 22:07:51 +0530

    Comment out Environment to fix 404 error

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -198,11 +198,11 @@ const TechStack = () => {
             />
           ))}
         </Physics>
-        <Environment
+        {/* <Environment
           files="/models/char_enviorment.hdr"
           environmentIntensity={0.5}
           environmentRotation={[0, 4, 2]}
-        />
+        /> */}
         <EffectComposer enableNormalPass={false}>
           <N8AO color="#0f002c" aoRadius={2} intensity={1.15} />
         </EffectComposer>
